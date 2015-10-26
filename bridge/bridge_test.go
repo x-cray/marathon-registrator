313fee514b9e59f8e5fbf05b9c4501ad88e56c5b
Denis Parchenko <denis.parchenko@gmail.com>
2015-10-27 00:12:59 +0200

    Add more unit tests

--- bridge/bridge_test.go
+++ bridge/bridge_test.go
@@ -1,6 +1,7 @@
 package bridge
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/x-cray/marathon-service-registrator/mocks"
@@ -22,6 +23,77 @@ func Test(t *testing.T) {
 		})
 
 		g.Describe("Sync()", func() {
+			g.It("Should forward errors received from RegistryAdapter.Services()", func() {
+				// Arrange.
+				registryAdapter.On("Services").Return([]*types.Service{}, errors.New("registry-error"))
+				bridge := &Bridge{
+					scheduler: schedulerAdapter,
+					registry:  registryAdapter,
+				}
+
+				// Act.
+				err := bridge.Sync()
+
+				// Assert.
+				registryAdapter.AssertExpectations(t)
+				g.Assert(err.Error()).Equal("registry-error")
+			})
+
+			g.It("Should forward errors received from RegistryAdapter.AdvertiseAddr()", func() {
+				// Arrange.
+				registryAdapter.On("Services").Return([]*types.Service{}, nil)
+				registryAdapter.On("AdvertiseAddr").Return("", errors.New("registry-error"))
+				bridge := &Bridge{
+					scheduler: schedulerAdapter,
+					registry:  registryAdapter,
+				}
+
+				// Act.
+				err := bridge.Sync()
+
+				// Assert.
+				registryAdapter.AssertExpectations(t)
+				g.Assert(err.Error()).Equal("registry-error")
+			})
+
+			g.It("Should forward errors received from SchedulerAdapter.Services()", func() {
+				// Arrange.
+				schedulerAdapter.On("Services").Return([]*types.Service{}, errors.New("scheduler-error"))
+				registryAdapter.On("Services").Return([]*types.Service{}, nil)
+				registryAdapter.On("AdvertiseAddr").Return("", nil)
+				bridge := &Bridge{
+					scheduler: schedulerAdapter,
+					registry:  registryAdapter,
+				}
+
+				// Act.
+				err := bridge.Sync()
+
+				// Assert.
+				schedulerAdapter.AssertExpectations(t)
+				g.Assert(err.Error()).Equal("scheduler-error")
+			})
+
+			g.It("Should do nothing if service sets in scheduler and registry are empty", func() {
+				// Arrange.
+				schedulerAdapter.On("Services").Return([]*types.Service{}, nil)
+				registryAdapter.On("Services").Return([]*types.Service{}, nil)
+				registryAdapter.On("AdvertiseAddr").Return("10.10.10.10", nil)
+				bridge := &Bridge{
+					scheduler: schedulerAdapter,
+					registry:  registryAdapter,
+				}
+
+				// Act.
+				bridge.Sync()
+
+				// Assert.
+				schedulerAdapter.AssertExpectations(t)
+				registryAdapter.AssertExpectations(t)
+				registryAdapter.AssertNotCalled(t, "Register")
+				registryAdapter.AssertNotCalled(t, "Deregister")
+			})
+
 			g.It("Should do nothing if service sets in scheduler and registry match", func() {
 				// Arrange.
 				schedulerServices := []*types.Service{
@@ -66,7 +138,7 @@ func Test(t *testing.T) {
 				registryAdapter.AssertNotCalled(t, "Deregister")
 			})
 
-			g.It("Should register 1 service absent in registry but present in scheduler", func() {
+			g.It("Should register 1 service absent from registry but present in scheduler", func() {
 				// Arrange.
 				schedulerServices := []*types.Service{
 					&types.Service{
@@ -112,7 +184,7 @@ func Test(t *testing.T) {
 				registryAdapter.AssertNotCalled(t, "Deregister")
 			})
 
-			g.It("Should register 2 services absent in registry but present in scheduler", func() {
+			g.It("Should register 2 services absent from registry but present in scheduler", func() {
 				// Arrange.
 				schedulerServices := []*types.Service{
 					&types.Service{
@@ -143,7 +215,7 @@ func Test(t *testing.T) {
 					} else {
 						g.Fail("Tried to register an unknown service: " + service.Name)
 					}
-				}).Return(nil)
+				}).Return(nil).Twice()
 				bridge := &Bridge{
 					scheduler: schedulerAdapter,
 					registry:  registryAdapter,
@@ -155,7 +227,46 @@ func Test(t *testing.T) {
 				// Assert.
 				schedulerAdapter.AssertExpectations(t)
 				registryAdapter.AssertExpectations(t)
-				registryAdapter.AssertNumberOfCalls(t, "Register", 2)
+				registryAdapter.AssertNotCalled(t, "Deregister")
+			})
+
+			g.It("Should not try to register services from address different than registry advertized one", func() {
+				// Arrange.
+				schedulerServices := []*types.Service{
+					&types.Service{
+						Name: "db-server",
+						IP:   "10.10.10.10",
+						Port: 27017,
+					},
+					&types.Service{
+						Name: "app-server",
+						IP:   "10.10.10.20",
+						Port: 3000,
+					},
+				}
+				registryServices := []*types.Service{}
+				schedulerAdapter.On("Services").Return(schedulerServices, nil)
+				registryAdapter.On("Services").Return(registryServices, nil)
+				registryAdapter.On("AdvertiseAddr").Return("10.10.10.10", nil)
+				registryAdapter.On("Register", mock.AnythingOfType("*types.Service")).Run(func(args mock.Arguments) {
+					service := args.Get(0).(*types.Service)
+
+					// Method call assertions.
+					g.Assert(service.Name).Equal("db-server")
+					g.Assert(service.IP).Equal("10.10.10.10")
+					g.Assert(service.Port).Equal(27017)
+				}).Return(nil).Once()
+				bridge := &Bridge{
+					scheduler: schedulerAdapter,
+					registry:  registryAdapter,
+				}
+
+				// Act.
+				bridge.Sync()
+
+				// Assert.
+				schedulerAdapter.AssertExpectations(t)
+				registryAdapter.AssertExpectations(t)
 				registryAdapter.AssertNotCalled(t, "Deregister")
 			})
 		})
